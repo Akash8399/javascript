bccac832ee4bad7e47136549a37f004e5b2157da
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-04-14 12:19:11 +0000

    config

---
+++ 01_basics/Strings.js
@@ -0,0 +1,18 @@
+const name = " Rax"
+const value = 70
+
+//console.log(name + " and roll no. " + value );    Basic Tarika
+
+console.log(`Hello, my name is ${name} and my roll no. is ${value}`); // Advance tarika 
+
+const gameName = new String('mahendre_singh_dhoni')
+
+console.log(gameName.charAt(7))
+console.log(gameName.indexOf('d'))
+console.log(gameName.toUpperCase())
+
+const newString = gameName.substring(0,15)   // Negetive value nhi de skte
+console.log(newString);
+
+const anotherString = gameName.substring(-14,6) //hiteshhc 
+console.log(anotherString);
