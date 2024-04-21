f25c27c10f341137eeb5f5eb946632f7951e754e
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-04-22 00:36:01 +0530

    Object part 2

--- 01_basics/07_objects.js
+++ 01_basics/07_objects.js
@@ -19,7 +19,7 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b", 3: "c"}
 const obj2 = {4: "d", 5: "e", 6: "f"}
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} //  Rest operator
 //console.log(obj3)
 
 console.log(tenderUser);
