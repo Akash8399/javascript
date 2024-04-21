ebb50a8878045eb7b3ae1484e4afa7b970baa44e
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-04-21 18:18:32 +0000

    Objects part 2

---
+++ 01_basics/07_objects.js
@@ -0,0 +1,33 @@
+//  const tenderUser = new Object()  -- Single term object
+
+const tenderUser = {}  // non single term object
+tenderUser.id = "123abc",
+tenderUser.name = "Sam",
+tenderUser.isLoggedIn = false
+
+//console.log(tenderUser);
+
+const regularUser = {
+    email : "sam@gmail.com",
+    fullname : {
+        firstname : "Sam",
+        lastname : "martin"
+    }
+}
+//console.log(regularUser.fullname.lastname);
+
+
+const obj1 = {1: "a", 2: "b", 3: "c"}
+const obj2 = {4: "d", 5: "e", 6: "f"}
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3)
+
+console.log(tenderUser);
+console.log(Object.keys(tenderUser));
+console.log(Object.values(tenderUser));
+console.log(Object.entries(tenderUser));
+console.log(tenderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
