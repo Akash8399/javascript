29c3dea6f00581cb2022f455918bcef0cc1f72f7
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-02 17:00:53 +0000

    Scopes

---
+++ 01_basics/09_Scopes.js
@@ -0,0 +1,32 @@
+function one(){
+    const name = "Akash"
+
+    function two(){
+        const website = "portfolio"
+        //console.log(name);
+    }
+    two()
+}
+one()
+
+if (true){
+    const name = "Akash"
+    if(name === "Akash"){
+        const website = " portfolio"
+        //console.log(`ny name is ${name} and my website is ${website}`);
+    }
+}
+
+
+//  **************** INTERESTING ********************
+
+function addone(num){
+    return num + 1
+}
+//addone(5)  // eaysa likhne se output to aayenga pr print nhi honga kyuki print kr ne bola hi nhi hai 
+console.log(addone(5))
+
+const addTwo = function(num){
+    return num + 5
+}
+console.log(addTwo(5));
