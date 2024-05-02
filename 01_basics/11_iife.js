9c2826cb94ee1213a54456abfbe7878c723d8190
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-02 19:17:07 +0000

    IIFE

---
+++ 01_basics/11_iife.js
@@ -0,0 +1,13 @@
+// Immediatly Invoked Function Expressions(IIFE)
+
+(function(){
+    console.log("dbms");
+} )();  // 1st IIFE file, (;)lagaya kyuki 2nd IIFE file execute hona chayhea
+
+((name) =>{
+    console.log(`dm me ${name}`);
+})("Akash") //  same chai("Akash") is hi tarha hai 
+
+
+
+
