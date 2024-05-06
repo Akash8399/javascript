652f1e3d22f0a70e1015551edc017dc8bb8077f1
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-06 20:17:16 +0000

    Do While and While Loop

---
+++ 03_iterations/two.js
@@ -0,0 +1,25 @@
+
+// While Loop
+
+let index = 0
+while(index <= 10){
+    //console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman", "ironman"]
+let arr = 0
+let val = 1
+while(arr < myArray.length){
+    //console.log(`Super hero ${val} is ${myArray[arr]}`);
+    arr = arr + 1;
+    val = val + 1;
+}
+
+//   Do While Loop
+
+let score = 1
+do {     // phle kam karo 
+    console.log(`score is ${score}`);
+    score++
+} while(score <= 10);   // fir condition veri kro
