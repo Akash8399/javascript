5fc005f60a4c84d9bb944ae04140b8c01d0fe500
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-06 18:52:38 +0000

    Switch case

---
+++ 02_control_flow/switch.js
@@ -0,0 +1,39 @@
+//   Two type of swith case (1) With Number
+
+//const month = 3
+//switch(month){
+//    case 1:
+//        console.log("January");
+//        break;
+//    case 2:
+//        console.log("February");
+//        break;
+//    case 3:
+//        console.log("March");
+//        break;
+//    case 4:
+//        console.log("April");
+//        break;
+//    default:
+//        console.log("default case match");
+//}
+
+// And (2) with String
+
+const month = "March"
+switch(month){
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("default case match");
+}
