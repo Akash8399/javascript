821f1ae9c90df4a1e7b48ce84c685fa583f0a9d1
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-07 15:36:57 +0000

    filter

---
+++ 03_iterations/six.js
@@ -0,0 +1,26 @@
+//   Method different but result same
+
+const myNums = [1,2,3,4,5,6,7,8]
+
+//const newNums = myNums.filter((item) => item > 3 )  // without object({})
+//console.log(newNums)
+
+const newNums = myNums.filter( (num) => {  // with object({})
+    return num > 5                         // scope laga ke kiya to return likhna imp hai 
+})
+//console.log(newNums);
+
+//  ** with for each loop
+const number = []
+myNums.forEach( (num) => {
+    if(num > 4){
+        number.push(num)
+    }
+})
+console.log(number);
+
+
+
+
+
+
