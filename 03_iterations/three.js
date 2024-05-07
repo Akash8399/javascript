52ab3c86d5b6a0b1ca57cdde650c95487f756777
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-07 09:37:30 +0000

    For Of and Maps

---
+++ 03_iterations/three.js
@@ -0,0 +1,30 @@
+// For of
+
+const arr = [1,2,3,4,5]
+for(const num of arr){
+    //console.log(num);
+}
+
+const greeting = "Hello, World"
+for (const greet of greeting){
+    //console.log(greet);
+}
+
+//  Maps
+
+const map = new Map()
+map.set("IN", "India")
+map.set("Np", "Nepal")
+map.set("Bra", "Brazil")
+//console.log(map);
+for (const [key,value] of map) {
+    console.log(key, ":-", value);  
+}
+
+const myObject = {
+    "game1": "PubG",
+    "game2": "FreeFire"
+}
+for(const [key,value] of myObject){
+    console.log(key, ":-", value)       // Output: TypeError: myObject is not iterable
+}
