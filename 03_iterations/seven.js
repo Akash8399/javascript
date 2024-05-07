f11df80ae9b6bc06f6abf0988c227d5f4e70a867
Akash8399 <101639103+Akash8399@users.noreply.github.com>
2024-05-07 17:26:41 +0000

    Map

--- 03_iterations/seven.js
+++ 03_iterations/seven.js
@@ -3,5 +3,8 @@
 const numbers = [1,2,3,4,5,6,7,8,9,10]
 //const myNums = numbers.map( (item) => item + 10)
 
-const myNum = numbers.map( (num) => num * 10 ).map(  (num) => num + 2).filter( (num) => num > 40)
+const myNum = numbers
+                .map( (num) => num * 10)
+                .map( (num) => num + 2)
+                .filter( (num) => num > 40)  // yeah teeno ek line me bhi likh sakte hai 
 console.log(myNum);
